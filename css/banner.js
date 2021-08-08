0fa118fd825af1b98a1cea82329228331cfa71b0
adbplane <3024196839@qq.com>
2021-08-08 22:02:56 +0800

    第一次上传博客模板

--- css/banner.js
+++
@@ -1,8 +0,0 @@
-<script>
-// JavaScript Document
-var m=12; //随机显示图片的总数量
-var n=Math.floor(Math.random()*m+1)
-document.write ("<img src='/img/1"+n+".jpg' border=0 galleryimg=no>");
-//图片保存在/banner/文件夹下，根据设置的图片总数量，分别放入10张从banner_01.gif到banner_10.gif的图片
-//在执行这个JS时，它会随机显示从banner_01.gif到banner_10.gif的图片。
-</script>
